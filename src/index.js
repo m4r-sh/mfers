c2b3c08290c976044b89c4f6a37b196bb2ca524f
m4r-sh <me@m4r.sh>
2025-06-12 21:29:31 -0500

    add oneofones export

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import { mferdata } from "./_data";
 import { Mfer } from './Mfer'
+import { uniques } from "./categories";
 
 export { describe } from "./describe";
 export { background_colors } from "./colors";
 export { traits } from "./traits";
-export { categories, categoryToTraits, traitToCategory, uniques } from "./categories";
+export { categories, categoryToTraits, traitToCategory } from "./categories";
 export { queryToFilter, filterToQuery } from "./filter";
 export { mfersToBuffer, traitsToB64, b64ToTraits } from "./encode";
 
 const mfers = initMfers()
+
+const oneofones = Object.keys(uniques).map(n => parseInt(n))
 
 function initMfers(){
   let arr = new Array(10021)
@@ -23,4 +26,4 @@ function findMfers(obj){
   return mfers.filter(mfer => mfer.match(obj))
 }
 
-export { mfers, findMfers }
+export { mfers, findMfers, oneofones, uniques }
--- tests/mfer.test.js
+++ tests/mfer.test.js
@@ -1,5 +1,5 @@
 import { test, expect } from 'bun:test'
-import { mfers } from '../src'
+import { mfers, oneofones } from '../src'
 
 test('mfer 0 API test', () => {
   expect([...mfers[0]]).toEqual([
@@ -14,4 +14,7 @@ test('mfer 0 API test', () => {
   expect(mfers[0].id).toBe(0)
   expect(mfers[0].toString()).toBe('[Mfer 0]')
   expect(JSON.stringify(mfers[0])).toBe('{"id":0,"traits":{"background":"red","type":"plain mfer","eyes":"regular eyes","hat under headphones":"knit buffalo","headphones":"black headphones","mouth":"smile","smoke":"cig black"}}')
+
+  expect(oneofones).toEqual([140,781,1825,2293,2506,3942,4482,5476,5659,5688,6551,7456,7503,8434,8618,9035,9205,9292,9547,9860,9967])
+
 })
